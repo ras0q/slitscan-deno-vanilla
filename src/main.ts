cf7cbc322f97acc8bb71ac483440ce2add667a0d
ras0q <ras0q@users.noreply.github.com>
2024-04-22 01:16:25 +0900

    :recycle: pre-create <img>

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,41 @@
+const framesCount = 500;
+
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = /* html */ `
+  <canvas id="canvas"></canvas>
   <div class="container">
-    <video
+    <div>
+      <video
       id="video"
       src="/AdobeStock_255856560.mp4"
-      width="40%"
-      height="auto"
       muted
-    ></video>
-    <div id="frames"></div>
+      loop
+      ></video>
+    </div>
+    <div id="frames">
+      ${`<img class="frame" />`.repeat(framesCount)}
+    </div>
   </div>
-  <canvas id="canvas"></canvas>
   <style>
+    .container {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      gap: 1rem;
+    }
+
+    #video {
+      margin: 0;
+      width: 100%;
+      height: auto;
+    }
+
     #canvas {
       display: none;
     }
 
     #frames {
       position: relative;
+      width: 100%;
+      height: 100%;
     }
 
     .frame {
@@ -40,14 +59,16 @@ video.addEventListener("play", () => {
       cancelAnimationFrame(frameID);
       return;
     }
+
     context?.drawImage(video, 0, 0, canvas.width, canvas.height);
-    const imgTag = document.createElement("img");
+    const imgTag = frames.children[i % framesCount] as HTMLImageElement;
     imgTag.className = "frame";
     imgTag.src = canvas.toDataURL("image/png");
-    imgTag.style.zIndex = `${i}`;
     // @ts-ignore object-view-box is now generally supported
-    imgTag.style.objectViewBox = `inset(0px ${i % canvas.width}px 0px 0px)`;
-    frames.appendChild(imgTag);
+    imgTag.style.objectViewBox = `inset(0px ${
+      (i * (canvas.width / framesCount)) % canvas.width
+    }px 0px 0px)`;
+
     i++;
 
     requestAnimationFrame(loop);
